69a2e69759f1ca12ac91e690a60bdfc53c750749
Oleg Voloshyn <voloshyn@active-bridge.com>
2015-07-12 15:48:19 +0300

    change deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :use_sudo, false
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, "/home/#{user}/olegv/#{application}"
+set :deploy_to, "/home/#{user}/olegv/event"
 
 # Default value for :scm is :git
 

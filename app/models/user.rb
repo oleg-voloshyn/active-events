5436429488e4188dcdeb19483d50c808b5652b93
Oleg Voloshyn <voloshyn@active-bridge.com>
2015-09-10 17:17:34 +0300

    update Rails version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    user = find_or_create_by(uid: auth[:uid], provider: auth[:provider])
-    user.provider = auth.provider
-    user.uid = auth.uid
-    user.name = auth.info.name
-    user.oauth_token = auth.credentials.token
-    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-    user.save!
-    user
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save(validate: false)
+    end
   end
 end

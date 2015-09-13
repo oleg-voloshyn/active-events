206ec52331bd0d87c88ca8620d4c79506d8d3859
Oleg Voloshyn <voloshyn@active-bridge.com>
2015-09-13 18:15:14 +0300

    add method helper "require_user"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_user
   helper_method :current_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    redirect_to root_path unless current_user
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
-  def show
-  end
+  skip_before_action :require_user
+
+  def show; end
 end

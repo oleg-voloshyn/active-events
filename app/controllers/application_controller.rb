ba62bc43222256f380cf28dbbd5934e47f86dde7
Oleg Voloshyn <voloshyn@active-bridge.com>
2015-09-16 10:48:15 +0300

    add helper 'date_link'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_user
   helper_method :current_user
+  helper_method :date_link
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
 
   def require_user
     redirect_to root_path unless current_user
+  end
+
+  def date_link(date)
+    if Date.current > date
+      "#{date.day}"
+    else
+      "<a data-remote='true' href='#{new_event_path}'>#{date.day}</a>".html_safe
+    end
   end
 end

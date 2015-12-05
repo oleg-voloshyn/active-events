9586405952cbf29ebfe5426ab322315560a7b87f
Oleg Voloshyn <voloshyn@active-bridge.com>
2015-12-05 23:04:38 +0200

    add validations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ActiveRecord::Base
   # geocoded_by :full_street_address
   # after_validation :geocode
-
-  # validates :name, :description, presence: true
+  validates :name, :description, presence: true
 end
